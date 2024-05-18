4d948acc3efc5056c64821f0c42b5b629c4d6bf2
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-05-18 12:56:29 -0400

    fix: :bug: Minor Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -10,23 +10,9 @@ export default async function IndexPage() {
       {/* Hero */}
       <div className="group top-0 mt-16 flex flex-col items-center justify-center px-1 py-6 md:sticky md:mt-0 md:px-4">
         <div className="flex flex-col items-center justify-center md:mt-0 md:w-96">
-          <a
-            className="mb-4 hidden w-fit p-0"
-            href="https://www.producthunt.com/posts/removerized?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-removerized"
-            target="_blank"
-          >
-            <Image
-              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=454170&theme=dark"
-              alt="removerized - Free&#0032;AI&#0045;Powered&#0032;Background&#0032;Remover&#0032;Tool | Product Hunt"
-              style={{ width: "170px", height: "34px" }}
-              width="170"
-              height="34"
-            />
-          </a>
-
           <h1 className="w-80 animate-fade-in-up text-center text-4xl font-bold md:text-start md:text-3xl lg:text-5xl">
             <span className="bg-gradient-to-br from-neutral-700 to-neutral-900 bg-clip-text text-transparent dark:from-neutral-50 dark:to-neutral-300">
-              Fast Image Optimization Tool{" "}
+              Fast Image Optimization Tool
             </span>
             <br></br>
             <span className="bg-gradient-to-br from-neutral-400 to-neutral-600 bg-clip-text text-transparent">
